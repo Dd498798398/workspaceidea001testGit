7e2992922d1a930306bcd888a6d507bbc0191ebf
zhanglaoshiglobal <498798398@qq.com>
2020-06-24 17:17:48 +0800

    测试冲突 2 覆盖  by zhanglaoshi

--- src/main/java/com/git/controller/Testgit02Controller.java
+++ src/main/java/com/git/controller/Testgit02Controller.java
@@ -3,6 +3,8 @@ package com.git.controller;
 public class Testgit02Controller {
 
     public String TestController() {
+
+        String a = "456";
         final String s = "123";
         return s;
     }
